9136f417c88446e0355d045536b20abd5c8636dc
Nicole Ochoa <ochoanefty@gmail.com>
2022-08-23 23:06:27 -0300

    Se agrefo JSON

--- script.js
+++ script.js
@@ -1,84 +1,53 @@
-const PRODUCTOS_HARDCODED = [
-    {
-    id:'1',
-    nombre: 'Soy el producto 1',
-    precio: 150,
-    descripcion:'Lorem ipsum dolor sit amet.',
-    img: 'https://pin.it/6hsB86khttps://pin.it/6hsB86k',
-    },
-    {
-    id:'2',
-    nombre: 'Soy el producto 2',
-    precio: 100,
-    descripcion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    img: 'https://pin.it/6hsB86khttps://pin.it/6hsB86k',
-    },
-    {
-    id:'3',
-    nombre: 'Soy el producto 3',
-    precio: 120,
-    descripcion:'Lorem ipsum dolor sit amet, consectetur',
-    img: 'https://pin.it/6hsB86khttps://pin.it/6hsB86k',
-    },
-    {
-    id:'4',
-    nombre: 'Soy el producto 4',
-    precio: 100,
-    descripcion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    img: 'https://pin.it/6hsB86khttps://pin.it/6hsB86k',
-    }        
-]
-class Producto{
-    constructor(id, nombre, precio, descripcion, img){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.descripcion = descripcion;
-        this.img = img;
-    }
-    obtenerInfo(){
-        return 'ID: ${this.id} | ${this.nombre} $${this.precio}';
-    }
-    obtenerAviso(emoji){
-        return '${this.nombre} >>>> valgo solo $${this.precio}${emoji}';
+class Apunte {
+    constructor(id, materia, descripcion, precio, img) {
+        this.id = id
+        this.materia = materia
+        this.descripcion = descripcion
+        this.precio = precio
+        this.img = img
     }
 }
-const carrito = [];
-const obtenerInfoProductos = (productosArray) => {
-    return productosArray.map( (elemento)=> elemento.obtenerAviso('😁')).join('\n');
-}
-const agregarAlCarritoById = (productos) => {
-const infoProductos = obtenerInfoProductos(productos);
-const id = prompt('Ingrese el ID del producto que desea agregar al carrito:\n' + infoProductos);
-const producto = productos.find((producto) => producto.id === id);
-if (!producto) return;
-carrito.push(producto);
-alert('Producto agregado al carrito');
-}
-const imprimirCarrito =(carritoDeProductos) =>{
-    carritoDeProductos.forEach((producto) => {
-        console.log(producto.obtenerAviso('💰'));
-    })
-}
-const obtenerTotal = (productosArray) => {
-    let total = 0;
-    productosArray.forEach((producto) => {
-        total += producto.precio;
-    });
-    return total;
-}
-const productos = PRODUCTOS_HARDCODED.map (producto => new Producto(
-    producto.id,
-    producto.nombre,
-    producto.precio,
-    producto.descripcion,
-    producto.img,
-));
 
-agregarAlCarritoById(productos);
-agregarAlCarritoById(productos);
-agregarAlCarritoById(productos);
-alert('Abre la consola para ver tu compra')
-imprimirCarrito (carrito);
-console.log ('TOTAL: ', obtenerTotal(carrito));
-//
+const apunte1 = new Apunte(1, "Administracion", "Material de catedra A", 500, "./LibroEnBlanco.jpg")
+const apunte2 = new Apunte(2, "Estados Contables", "Resumen de cursada y tps", 300, "./LibroEnBlanco.jpg")
+const apunte3 = new Apunte(3, "Matematica para Economistas", "Apunte de la materia", 1000, "./LibroEnBlanco.jpg")
+const apunte4 = new Apunte(4, "Introduccion a la economia", "Apuntes de catedra A", 250, "./LibroEnBlanco.jpg")
+const apunte5 = new Apunte(5, "Contabilidad I", "Resumen para final", 800, "../LibroEnBlanco.jpg")
+
+const apuntes = [apunte1, apunte2, apunte3, apunte4, apunte5]
+
+const tBody = document.querySelector('#tBodyApuntes')
+apuntes.forEach(apuntesArray => {
+    tBody.innerHTML += `
+        <tr>
+            <th scope="row">${apuntesArray.id}</th>
+            <td>${apuntesArray.materia}</td>
+            <td>${apuntesArray.descripcion}</td>
+            <td>${apuntesArray.precio}</td>
+            <td><img src="${apuntesArray.img}"></td>
+        </tr>
+    `
+})
+localStorage.setItem('carrito', JSON.stringify(apuntes))
+console.log(JSON.parse(localStorage.getItem("carrito")))
+class User {
+    constructor(username, email, password) {
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+}
+const idForm = document.getElementById("idForm")
+const botonUsers = document.getElementById("botonUsers")
+const divUsers = document.getElementById("divUsers")
+const users  = []
+idForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const username = documento.getElementById("username").value
+    const email = documento.getElementById("email").value
+    const password = documento.getElementById("password").value
+    const user = new User(username, email, password)
+    users.push(user)
+    idForm.reset()
+    console.log(users)
+})
